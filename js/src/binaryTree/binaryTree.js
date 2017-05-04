85a46748a80161560c81dfe1fa18d8c4f2cb1125
DESKTOP-AKPH504\tiagofabre <tiagofabre@gmail.com>
2017-05-03 22:58:45 -0300

    'delete'

--- js/src/binaryTree/binaryTree.js
+++ js/src/binaryTree/binaryTree.js
@@ -4,6 +4,19 @@ class TreeNode {
         this.left = null;
         this.right = null;
         this.data = _data;
+        this.parent = null;
+    }
+
+    hasTwoChilds() {
+        return this.left != null && this.right != null;
+    }
+
+    hasOnlyLeftChild() {
+        return this.left != null && this.right === null;
+    }
+
+    hasOnlyRightChild() {
+        return this.right != null && this.left === null;
     }
 
     getLeftNode() {
@@ -17,22 +30,27 @@ class TreeNode {
     getValue() {
         return this.data;
     }
+
+    getParent() {
+
+    }
 }
 
 module.exports = class BinaryTree {
 
     constructor() {
         this.root = null;
+        this.nodeCount = 0;
     }
 
     insert(value) {
         let node = new TreeNode(value);
+        this.nodeCount++;
 
         if (!this.root) {
             this.root = node;
         } else {
             let searchResult = this.search(value, this.root);
-            console.log(searchResult);
             if (value < searchResult.getValue()) {
                 searchResult.left = node;
             } else {
@@ -41,9 +59,59 @@ module.exports = class BinaryTree {
         }
     }
 
+    deleteNode(value) {
+        let node = this.search(value, this.root);
+
+        // Object didn't find
+        if(node.getValue() != value)
+            return -1;
+
+        if (!node.hasTwoChilds()) {
+            if ( value < node.parent.getValue()) {
+                node.parent.left = null;
+            } else {
+                node.parent.right = null;
+            }
+        } else if (node.hasTwoChilds()) {
+        
+            // find the next nearest number from value
+            let currentNode = node.right, movedLeft = false;
+            while(currentNode.left) {
+                currentNode = currentNode.left;
+                movedLeft = true;
+            }
+
+            // after find it we remove this leaf to replace the deleted node
+            if (movedLeft)
+                currentNode.parent.left = null;
+            else
+                currentNode.parent.right = null;
+
+            // replacing
+            if ( value < node.parent.getValue()) {
+                node.parent.left = currentNode;
+            } else {
+                node.parent.right = currentNode;
+            }
+            
+            currentNode.left = node.left;
+            currentNode.right = node.right;
+
+
+        } else if (node.hasOnlyLeftChild() || node.hasOnlyRightChild()) {
+            if ( value < node.parent.getValue()) {
+                node.parent.left = node.left;
+            } else {
+                node.parent.right = node.right;
+            }
+        }
+    } 
+
     search(value, node) {
 
-        if (value == node.getValue()) {
+        if (!node) {
+            return -1;
+        } else if (value == node.getValue()) {
             return node;
         } else if (value < node.getValue()) {
             if (node.getLeftNode() == null)
@@ -53,7 +121,7 @@ module.exports = class BinaryTree {
         } else {
             if (node.getRightNode() == null)
                 return node;
-            else 
+            else
                 return this.search(value, node.getRightNode());
         }
     }
--- js/src/binaryTree/binaryTree.spec.js
+++ js/src/binaryTree/binaryTree.spec.js
@@ -9,5 +9,29 @@ let binaryTree = new BinaryTree();
         binaryTree.insert(8)
         binaryTree.insert(10)
         binaryTree.insert(5)
+        binaryTree.insert(6)
+        binaryTree.insert(7)
+        binaryTree.insert(8)
+        binaryTree.insert(9)
+        binaryTree.insert(10)
+        binaryTree.insert(11)
+
+        binaryTree.insert(80)
+        binaryTree.insert(79)
+        binaryTree.insert(78)
+        binaryTree.insert(77)
+
+        binaryTree.insert(3)
+        binaryTree.insert(36)
+        binaryTree.insert(12)
+
+        binaryTree.search(4)
+
+        binaryTree.search(67)
+
+        binaryTree.search(-1)
+
+        binaryTree.deleteNode(4)
+        binaryTree.deleteNode(80)
 //     })
 // })
